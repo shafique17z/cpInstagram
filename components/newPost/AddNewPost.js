3a2d4843bc49c073800084d8ef5a59e271c565e8
Shafique Ahmed <shafiqued17z@gmail.com>
2023-03-05 23:11:36 +0500

    On Formik Uploader Now

--- components/newPost/AddNewPost.js
+++ components/newPost/AddNewPost.js
@@ -1,17 +1,44 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-function AddNewPost() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Add New Post</Text>
-    </View>
-  );
-}
+const AddNewPost = () => (
+  <View style={styles.container}>
+    <Header />
+  </View>
+);
+
+const Header = () => (
+  <View style={styles.headerContainer}>
+    <TouchableOpacity>
+      <Image
+        source={{
+          uri: "https://img.icons8.com/ios-glyphs/90/ffffff/back.png",
+        }}
+        style={{
+          width: 30,
+          height: 30,
+        }}
+      />
+    </TouchableOpacity>
+    <Text style={styles.headerText}>NEW POST</Text>
+    <Text></Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
-  text: {
+  container: {
+    marginHorizontal: 10,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerText: {
     color: "white",
+    fontSize: 20,
+    fontWeight: "700",
+    marginRight: 20,
   },
 });
 
